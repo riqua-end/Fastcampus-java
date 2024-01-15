33dae7a95c86a03c355a8b7d35f9f512c0210edc
Kim KJ <riqua1@naver.com>
2024-01-15 14:05:12 +0900

    Java 복습 - 관계(비교)연산자 , 논리연산자

--- src/ArithmaticOperTest.java
+++ src/ArithmaticOperTest.java
@@ -31,5 +31,47 @@ public class ArithmaticOperTest {
         //a %= b    ---->  a = a%b
 
 
+
+
+        //관계(비교) 연산자 ( == , != , < , > , <= , >= )
+        //연산의 결과는 True or false
+
+        // Q. 관계(비교)연산자의 결과 값을 쓰시오
+        int a = 4;
+
+        System.out.println(a>5); //false
+        System.out.println(a<5); //true
+        System.out.println(a==6); //false
+        System.out.println(a!=4); //false
+        System.out.println(3>=a); //false
+        System.out.println(a<=a+1); //true
+
+        // ( instanceof ) , 부모와 자식 간의 상속 관계인지 아닌지 확인할 때 사용 , true or false
+
+        System.out.println("논리 연산자");
+
+        // 논리 연산자 ( && , || , ! ) 결과는 true or false
+        // && = and 연산, || = or 연산 , ! = not 연산
+        // !a = a가 참이면 거짓 , 거짓이면 참
+        // a && b = a,b가 둘 다 참이면 참,그렇지 않으면 거짓
+        // a || b = a,b 중 하나라도 참이면 참 , 그렇지 않으면 거짓
+
+
+        // Q. 논리 연산자의 결과 값을 쓰시오
+
+        int x = 1 , y = 0;
+        System.out.println(!(x>0)); // false
+        System.out.println(x>0&&x<3); //true
+        System.out.println(x>0&&y<0); //false
+        System.out.println(x<0||y>=0); //true
+        System.out.println(y<0&&y<-5); //false
+
+        System.out.println();
+
+        int c = 3, d = 5;
+        System.out.println((c>=3)&&(d<6)); //true
+        System.out.println((c!=3)&&(c>2)); //false
+        System.out.println((c!=3)||(c>2)); // true
+        System.out.println((d!=5)||(c==1)); //false
     }
 }
