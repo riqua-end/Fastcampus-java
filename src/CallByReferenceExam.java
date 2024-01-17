7b7a5b4a2c663d93b6ac3f57d30abef1a9ba97a2
Kim KJ <riqua1@naver.com>
2024-01-17 11:55:05 +0900

    Java 복습 - Method의 전달기법

---
+++ src/CallByReferenceExam.java
@@ -0,0 +1,19 @@
+public class CallByReferenceExam {
+    public static void main(String[] args) {
+        int[] a = { 1,2,3,4,5 };
+
+        // 배열의 총합을 구하여 출력하세요.
+
+        int result = addArray(a); // 번지 전달(Call By Reference)
+        System.out.println("result = " + result);
+    }
+    // Q. 매개변수로 정수형 배열을 받아서 / 배열의 총합을 구하여 / 리턴하는 / 메서드를 정의 하세요.
+
+    public static int addArray(int[] a){
+        int sum = 0;
+        for (int i = 0 ; i <a.length ; i++) {
+            sum=sum+a[i]; // sum+=a[i];
+        }
+        return sum;
+    }
+}
