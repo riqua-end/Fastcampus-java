2fa583bca97520ea3d71f815f876adf594edec73
Kim KJ <riqua1@naver.com>
2024-01-18 13:58:14 +0900

    Java 복습 - 객체 생성 , 객체 지향 측면의 클래스  (멤버 변수,멤버 메서드)

---
+++ src/fc/java/part3/BookDTO.java
@@ -0,0 +1,11 @@
+package fc.java.part3; // 패키지 선언문
+// fc.java.part3.BookDTO : 클래스의 풀네임
+public class BookDTO {
+    // 멤버 변수의 생성
+    public String title;
+    public int price;
+    public String company;
+    public String author;
+    public int page;
+    public String isbn;
+}
