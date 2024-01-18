a19516aff2e484e39a255db9b09ac1329b23bab1
Kim KJ <riqua1@naver.com>
2024-01-18 14:16:15 +0900

    Java 복습 - 객체 생성 과정의 이해

---
+++ src/fc/java/part3/PersonTest.java
@@ -0,0 +1,17 @@
+package fc.java.part3;
+
+public class PersonTest {
+    public static void main(String[] args) {
+        // Q. 한사람의 데이터를 저장 할 변수를 선언하세요.
+//        Person p;
+
+        // Q. 사람의 이름 , 나이, 전화번호를 저장할 [객체를 생성]하세요.
+//        p=new Person(); // new 연산자를 사용해 만들어진 객체가 메모리에서 할당된 것을 인스턴스라고 함.
+
+        // Q. 모델링 된 Person클래스를 이용하여 [객체를 메모리에 생성]하세요.
+        // [ 인스턴스를 만드는 과정 ]
+        Person p = new Person();
+
+
+    }
+}
