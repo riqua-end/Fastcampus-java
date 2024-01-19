0fa67c4f582e8471507943ffe53184765d9eeaf3
Kim KJ <riqua1@naver.com>
2024-01-19 13:26:53 +0900

    Java 복습 - Model의 종류,DTO,VO가 필요한 이유

---
+++ src/fc/java/model/CarDTO.java
@@ -0,0 +1,10 @@
+package fc.java.model;
+
+public class CarDTO { // 데이터를 하나로 묶어야 될 경우, 데이터를 하나로 수집하는 역할
+    public int carSn;
+    public String carName;
+    public int carPrice;
+    public String carOwner;
+    public int carYear;
+    public String carType;
+}
