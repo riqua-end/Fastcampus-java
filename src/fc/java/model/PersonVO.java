fc64841494193b8d7a917a25ef2c5ef9b80a5467
Kim KJ <riqua1@naver.com>
2024-01-21 15:41:15 +0900

    Java 복습 - setter,getter 메서드

--- src/fc/java/model/PersonVO.java
+++ src/fc/java/model/PersonVO.java
@@ -1,7 +1,32 @@
 package fc.java.model;
 
 public class PersonVO {
-    public String name;
-    public int age;
-    public String phone;
+    private String name;
+    private int age;
+    private String phone;
+
+    // Setter , Getter 메서드 만들기
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
 }
