e1a78ab1af73f45910ee268c344369fc755aea53
Kim KJ <riqua1@naver.com>
2024-01-30 15:01:21 +0900

    Java 복습 - 최상위 클래스 Object

---
+++ src/fc/java/poly/A.java
@@ -0,0 +1,11 @@
+package fc.java.poly;
+import java.lang.*;
+public class A extends Object{
+
+    public A(){
+        super();
+    }
+    public void display(){
+        System.out.println("나는 A 입니다.");
+    }
+}
