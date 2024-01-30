6d2d6174cc3702734fd91337b1c928b4e0d5370b
Kim KJ <riqua1@naver.com>
2024-01-30 15:20:38 +0900

    Java 복습 - Object를 이용한 다형성 활용

--- src/fc/java/poly/A.java
+++ src/fc/java/poly/A.java
@@ -8,4 +8,8 @@ public class A extends Object{
     public void display(){
         System.out.println("나는 A 입니다.");
     }
+
+    public void printGo(){
+        System.out.println("나는 A 입니다.");
+    }
 }
